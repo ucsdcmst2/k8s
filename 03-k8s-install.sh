1cc03d33eaa9cf765b291285ac3c7cc5cf41b9a9
aashayarora <aaarora@ucsd.edu>
2025-11-04 17:38:28 -0800

    update

--- 03-k8s-install.sh
+++ 03-k8s-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-KUBERNETES_VERSION=1.30
+KUBERNETES_VERSION=1.32
 
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
 
 apt update -y
-apt install -y kubelet=1.30.0-1.1 kubectl=1.30.0-1.1 kubeadm=1.30.0-1.1
+apt install -y kubelet=${KUBERNETES_VERSION}.0-* kubectl=${KUBERNETES_VERSION}.0-* kubeadm=${KUBERNETES_VERSION}.0-*
 
 apt-mark hold kubelet kubeadm kubectl
 
--- 04-controller-config.sh
+++ 04-controller-config.sh
@@ -5,7 +5,7 @@ IPADDR=$(curl -s ifconfig.me)
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
 
-kubeadm init --control-plane-endpoint=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME
+kubeadm init --kubernetes-version=1.32.0 --control-plane-endpoint=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
