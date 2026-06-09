db4533f084e8f0ee9560537457ce4912031ac58e
aashayarora <42879633+aashayarora@users.noreply.github.com>
2026-06-09 12:35:36 -0700

    Modify Calico installation for dual-stack support
    Updated Calico installation script to use custom dual-stack configuration and changed namespace for calico-node pods.

--- 05-cni-calico.sh
+++ 05-cni-calico.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 set -e
 
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.31.3/manifests/tigera-operator.yaml
 
-kubectl wait --for=condition=ready pod -l k8s-app=calico-node -n kube-system --timeout=300s || true
+# Apply custom Installation with dual-stack instead of the default
+cat <<EOF | kubectl create -f -
+apiVersion: operator.tigera.io/v1
+kind: Installation
+metadata:
+  name: default
+spec:
+  calicoNetwork:
+    bgp: Enabled
+    hostPorts: Enabled
+    ipPools:
+    - allowedUses:
+      - Workload
+      - Tunnel
+      assignmentMode: Automatic
+      blockSize: 26
+      cidr: 192.168.0.0/16
+      encapsulation: VXLANCrossSubnet
+      name: default-ipv4-ippool
+      natOutgoing: Enabled
+      nodeSelector: all()
+    - allowedUses:
+      - Workload
+      - Tunnel
+      assignmentMode: Automatic
+      blockSize: 122
+      cidr: fd00::/48
+      encapsulation: None
+      name: default-ipv6-ippool
+      natOutgoing: Enabled
+      nodeSelector: all()
+    nodeAddressAutodetectionV4:
+      firstFound: true
+    nodeAddressAutodetectionV6:
+      firstFound: true
+EOF
 
-kubectl get pods -n kube-system -l k8s-app=calico-node
-kubectl get pods -n kube-system -l k8s-app=calico-kube-controllers
+# Wait for calico-node pods in calico-system namespace (tigera-operator uses calico-system, not kube-system)
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=calico-node -n calico-system --timeout=300s || true
+
+kubectl get pods -n calico-system -l app.kubernetes.io/name=calico-node
+kubectl get pods -n calico-system -l app.kubernetes.io/name=calico-kube-controllers
+
+# Enable auto host endpoints
+kubectl patch kubecontrollersconfiguration default --patch='{"spec": {"controllers": {"node": {"hostEndpoint": {"autoCreate": "Enabled"}}}}}'
