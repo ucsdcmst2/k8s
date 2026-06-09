53d5966c4fab09acf4595e5c61fb8854f90df732
aashayarora <42879633+aashayarora@users.noreply.github.com>
2026-06-09 12:32:40 -0700

    Add IPv6 support and update kubeadm init command

--- 04-controller-config.sh
+++ 04-controller-config.sh
@@ -3,9 +3,20 @@ set -e
 
 IPADDR=$(curl -s ifconfig.me)
 NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
+POD_CIDR="192.168.0.0/16,fd00::/48"
+SERVICE_CIDR="10.96.0.0/12,fd00:1::/108"
 
-kubeadm init --kubernetes-version=1.32.0 --control-plane-endpoint=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME
+# Detect primary interface IPv6
+PRIMARY_INTERFACE=$(ip route | grep default | awk '{print $5}' | head -n1)
+IPV6ADDR=$(ip --json addr show $PRIMARY_INTERFACE | jq -r '.[0].addr_info[] | select(.family == "inet6" and .scope == "global" and (.local | startswith("fe80") | not)) | .local' | head -n1)
+
+kubeadm init \
+  --kubernetes-version=1.32.0 \
+  --control-plane-endpoint=$IPADDR \
+  --apiserver-cert-extra-sans=$IPADDR,$IPV6ADDR \
+  --pod-network-cidr=$POD_CIDR \
+  --service-cidr=$SERVICE_CIDR \
+  --node-name $NODENAME
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
